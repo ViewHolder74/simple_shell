b395a81a1f6f018bb83dc5bafd0dd6bb7879ccb3
Mussa Milanzi <viewholder74@gmail.com>
2023-09-09 07:17:06 -0700

    main.c

--- main.c
+++ main.c
@@ -12,22 +12,47 @@
 
 int main(int argc, char **argv)
 {
-	char *buffer, *lineptr;
+	char *buffer = "$ ", *lineptr = NULL;
 	size_t n = 0;
 	ssize_t num_char;
+	pid_t pid;
+	int status;
 
-	buffer = "<shell> $ ";
+	(void)argc; (void)argv;
 
 	while (1)
 	{
 		printf("%s", buffer);
+		fflush(stdout);
 		num_char = getline(&lineptr, &n, stdin);
 		if (num_char == -1)
 		{
-			return (-1);
+			printf("\n");
+			break;
 		}
-
-		printf("%s\n", lineptr);
+		if (num_char > 0 && lineptr[num_char -1] == '\n')
+			lineptr[num_char - 1] = '\0';
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("Fork failed");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			char *arg_passed[] = {lineptr, NULL};
+			execve(lineptr, arg_passed, NULL);
+			perror("Error:");
+			exit(1);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			if (status == 0)
+			{
+				continue;
+			}
+		}
 	}
 	free(lineptr);
 
