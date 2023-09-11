aa087bd7ab9fb084a6d42b452a3172ba26386195
Mussa Milanzi <viewholder74@gmail.com>
2023-09-11 10:56:51 -0700

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <errno.h>
 #define MAX_COMMAND_LENGTH 1024
+
+/**
+ * lineptr_exec - function that executes cmd
+ * @lineptr: command to execute
+ * @args: argument vector
+ * @envp: environment variable
+ * Return: 0 if successful, -1 if error
+ */
+int lineptr_exec(char *lineptr, char **args, char **envp)
+{
+	char *path = getenv("PATH");
+	char *pathCpy = strdup(path);
+	char *token, **dir, *delim = ":";
+	char full_path[MAX_COMMAND_LENGTH];
+
+	if (strcmp(lineptr, "env") == 0)
+	{
+		dir = envp;
+		while (*dir != NULL)
+		{
+			printf("%s\n", *dir);
+			dir++;
+		}
+		return (0);
+	}
+	if (pathCpy == NULL)
+	{
+		perror("Failed to allocate memory");
+		return (-1);
+	}
+	token = strtok(pathCpy, delim);
+	while (token != NULL)
+	{
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, lineptr);
+		execve(full_path, args, envp);
+		if (errno != ENOENT)
+		{
+			perror("Error");
+			free(pathCpy);
+			return (-1);
+		}
+		token = strtok(NULL, delim);
+	}
+	free(pathCpy);
+	return (-1);
+}
+
 /**
  * main - defines a unix interprater
  * @argc: argment count
@@ -10,15 +60,14 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
-	char *buffer = "$ ", *lineptr = NULL;
+	char *buffer = "$ ", *token;
+	char *lineptr = NULL, *delim = " ";
+	char *args[MAX_COMMAND_LENGTH];
 	size_t n = 0;
 	ssize_t num_char;
-	pid_t pid;
-	int status, count = 0;
-
-	(void)argc; (void)argv;
+	int result, count;
 
 	while (1)
 	{
@@ -32,36 +81,17 @@ int main(int argc, char **argv)
 		}
 		if (num_char > 0 && lineptr[num_char - 1] == '\n')
 			lineptr[num_char - 1] = '\0';
-		pid = fork();
-		if (pid < 0)
+		token = strtok(lineptr, delim);
+		for (count = 0; token != NULL; count++)
 		{
-			perror("Fork failed");
-			exit(1);
+			args[count] = token;
+			token = strtok(NULL, delim);
 		}
-		else if (pid == 0)
-		{
-			char *args[MAX_COMMAND_LENGTH];
-			char *token = strtok(lineptr, " ");
-			for (count = 0; token != NULL; count++)
-			{
-				args[count] = token;
-				token = strtok(NULL, " ");
-			}
-			args[count] = NULL;
-			execve(args[0], args, NULL);
-			perror("Error:");
-			exit(1);
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-			if (status == 0)
-			{
-				continue;
-			}
-		}
+		args[count] = NULL;
+		result = lineptr_exec(args[0], args, envp);
+		if (result == -1)
+			perror("Error");
 	}
 	free(lineptr);
-
 	return (0);
 }
