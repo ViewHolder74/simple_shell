046ba2514ec230b42ab26100509ad764ca6cebb1
Mussa Milanzi <viewholder74@gmail.com>
2023-09-11 12:23:54 -0700

    simple_shell

--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 #include <errno.h>
 #define MAX_COMMAND_LENGTH 1024
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src);
+int lineptr_exec(char *lineptr, char **args, char **envp);
 
 #endif
