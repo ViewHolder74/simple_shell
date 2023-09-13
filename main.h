00fe5394b2ba3bb1211fab85545d2f493f85c316
AkosuaTakyiwaa <nhyiradenkyi@gmail.com>
2023-09-13 10:28:48 -0700

    update

--- main.h
+++ main.h
@@ -9,8 +9,13 @@
 #include <errno.h>
 #define MAX_COMMAND_LENGTH 1024
 
+/**
+ * _strdup - a function that duplicates string
+ * _strcmp - a function that compares two strings
+ */
 char *_strcpy(char *dest, char *src);
 int lineptr_exec(char *lineptr, char **args, char **envp);
 int _strcmp(char *s1, char *s2)
+char *_strdup(char *str)
 
 #endif
